5370b9170a121f7fb51b466230a503dcdab5e524
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2015-04-26 17:05:31 +0900

    作成

---
+++ Project/TestScenario/AddTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestScenario
+{
+    [TestClass]
+    public class AddTest : TestBase<AddTest>
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext c)
+        {
+            NotifyClassInitialize();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            NotifyClassCleanup();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            NotifyTestInitialize();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            NotifyTestCleanup();
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            var addForm = App.MainForm.ButtonAdd_EmulateClick();
+            addForm.TextBoxName.EmulateChangeText("ishikawa-tatsuya");
+            addForm.TextBoxAddress.EmulateChangeText("Japan");
+            addForm.RadioButtonMan.EmulateCheck();
+            addForm.ButtonEntry_EmulateClickAndClose();
+            Assert.AreEqual("ishikawa-tatsuya(男) Japan", App.MainForm.ListBoxEmployee_GetItemText(0));
+        }
+
+        class ErrorParam
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public bool Checked { get; set; }
+            public string Message { get; set; }
+        }
+
+        [TestMethod]
+        [DataSource("System.Data.OleDB",
+            @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=Params.xlsx; Extended Properties='Excel 12.0;HDR=yes';",
+            "AddTest$",
+            DataAccessMethod.Sequential
+        )]     
+        public void TestError()
+        {
+            var param = GetParam<ErrorParam>();
+            var addForm = App.MainForm.ButtonAdd_EmulateClick();
+            addForm.TextBoxName.EmulateChangeText(param.Name);
+            addForm.TextBoxAddress.EmulateChangeText(param.Address);
+            if (param.Checked)
+            {
+                addForm.RadioButtonMan.EmulateCheck();
+            }
+            Assert.AreEqual(param.Message, addForm.ButtonEntry_EmulateClickAndGetMessage());
+            addForm.Close();
+        }
+    }
+}
---
+++ Project/TestScenario/TestBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeManagementDriver;
+using System.Collections.Generic;
+using EmployeeManagement;
+using System.Linq;
+
+namespace TestScenario
+{
+    public class TestBase<T>
+    {
+        static protected AppDriver App { get; set; }
+        static Dictionary<string, bool> _tests;
+        public TestContext TestContext { get; set; }
+
+        public static void NotifyClassInitialize()
+        {
+            App = new AppDriver();
+            _tests = typeof(T).GetMethods().Where(e => 0 < e.GetCustomAttributes(typeof(TestMethodAttribute), true).Length).ToDictionary(e => e.Name, e => true);
+        }
+
+        public static void NotifyClassCleanup()
+        {
+            App.EndProcess();
+        }
+
+        public void NotifyTestInitialize()
+        {
+            App.Attach();
+        }
+
+        public void NotifyTestCleanup()
+        {
+            _tests.Remove(TestContext.TestName);
+            App.Release(TestContext.CurrentTestOutcome == UnitTestOutcome.Passed && 0 < _tests.Count);
+        }
+
+        public Data GetParam<Data>() where Data : new()
+        {
+            Data data = new Data();
+            foreach (var e in typeof(Data).GetProperties())
+            {
+                e.GetSetMethod().Invoke(data, new object[] { Convert(e.PropertyType, TestContext.DataRow[e.Name]) });
+            }
+            return data;
+        }
+
+        static object Convert(Type type, object obj)
+        {
+            string value = obj == null ? string.Empty : obj.ToString();
+            if (type == typeof(int))
+            {
+                return int.Parse(value);
+            }
+            else if (type == typeof(bool))
+            {
+                return string.Compare(value, true.ToString(), true) == 0;
+            }
+            else if (type == typeof(string))
+            {
+                return value;
+            }
+            throw new NotSupportedException();
+        }
+    }
+}
